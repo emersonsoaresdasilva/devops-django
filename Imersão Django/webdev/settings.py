7b9eb4b7e0af190f933fcb2ca5e6878fed67de57
Emerson Soares <emerson.soares@aluno.faculdadeimpacta.com.br>
2021-03-25 15:37:09 -0300

    Ajustando bug de Allowed Hosts

--- Imersão Django/webdev/settings.py
+++ Imersão Django/webdev/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'sv$9u@f5si)w$v!crkant3la6nxf8o!6azr9d@qhgvym=)9)+3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
