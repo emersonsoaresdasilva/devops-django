5a4125cd9c6f15dbe2dda87cc6b28f1b2a1ba5ed
Emerson Soares <emerson.soares@aluno.faculdadeimpacta.com.br>
2021-04-03 03:05:22 -0300

    Implementada lista de tarefas pendentes

--- Imersão Django/webdev/tarefas/testes/test_tarefas_get.py
+++ Imersão Django/webdev/tarefas/testes/test_tarefas_get.py
@@ -2,9 +2,11 @@ import pytest
 from django.urls import reverse
 from pytest_django.asserts import assertContains
 
+from webdev.tarefas.models import Tarefa
+
 @pytest.fixture
-def resposta(client):
-    return client.get(reverse('tarefas:home'))
+def resposta(client, db):
+    return (client.get(reverse('tarefas:home')))
 
 def test_status_code(resposta):
     assert (resposta.status_code == 200)
@@ -14,3 +16,21 @@ def test_formulario_presente(resposta):
 
 def test_botao_salvar_presente(resposta):
     assertContains(resposta, '<button type="submit"')
+
+@pytest.fixture
+def lista_de_tarefas_pendentes(db):
+    tarefas = [
+        Tarefa(nome='Tarefa 01', feita=False),
+        Tarefa(nome='Tarefa 02', feita=False),
+        Tarefa(nome='Tarefa 03', feita=False)
+    ]
+    Tarefa.objects.bulk_create(tarefas)
+    return tarefas
+
+@pytest.fixture
+def resposta_com_lista_de_tarefas(client, lista_de_tarefas_pendentes):
+    return (client.get(reverse('tarefas:home')))
+
+def test_lista_de_tarefas_pendentes_presentes(resposta_com_lista_de_tarefas, lista_de_tarefas_pendentes):
+    for tarefa in lista_de_tarefas_pendentes:
+        assertContains(resposta_com_lista_de_tarefas, tarefa.nome)
--- Imersão Django/webdev/tarefas/views.py
+++ Imersão Django/webdev/tarefas/views.py
@@ -1,8 +1,11 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+
+# Create your views here.
 from django.urls import reverse
 
 from webdev.tarefas.forms import TarefaNovaForm
+from webdev.tarefas.models import Tarefa
 
 def home(request):
     if request.method == 'POST': # Caso os dados do formulário serem válidos.
@@ -11,5 +14,7 @@ def home(request):
             form.save() # Salva no banco de dados.
             return HttpResponseRedirect(reverse('tarefas:home')) # Redireciona para home de tarefas.
         else: # Não são válidos?
-            return (render(request, 'tarefas/home.html', {'form': form}, status=400))
-    return render(request, ('tarefas/home.html'))
+            tarefas_pendentes = Tarefa.objects.filter(feita=False).all()
+            return render(request, 'tarefas/home.html', {'form': form, 'tarefas_pendentes': tarefas_pendentes}, status=400)
+    tarefas_pendentes = Tarefa.objects.filter(feita=False).all()
+    return render(request, 'tarefas/home.html', {'tarefas_pendentes': tarefas_pendentes})
